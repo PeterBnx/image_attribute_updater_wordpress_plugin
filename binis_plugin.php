39fb44c6af55ecf4cf389a7d8cbe8b8a9672b17a
Peter <binispeter@gmail.com>
2025-07-23 19:52:30 +0300

    initial commit

---
+++ binis_plugin.php
@@ -0,0 +1,9 @@
+<?php
+/*
+ * Plugin Name: Binis Plugin
+ * Plugin URI:  https://binis.gr/plugins/binis-plugin
+ * Description: Altering products' alt texts into the corresponding title
+ * Version:     1.0
+ * Requires 
+ */
+?>
